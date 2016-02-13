896ef685dd146081e786cd74e1f0361ef7195cd3
Sidney_Primas_2014_Personal_Computer <sidney.primas@gmail.com>
2016-02-12 23:34:11 -0500

    Completed Part IV

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -24,36 +24,62 @@ func (mr *Master) schedule(phase jobPhase) {
 	// multiple tasks.
 	
 	var current_task int = 0
+	failChannel := make(chan *DoTaskArgs)
+
 	for current_task < ntasks {
 
 	
 		select {
 		//Send a task to a worker
 		case idle_worker := <-mr.registerChannel:
+
+			select {
+			case failTaskArgs := <- failChannel:
+				go func(myArgs *DoTaskArgs, myWorker string) {
+
+					var reply ShutdownReply
+					ok := call(myWorker, "Worker.DoTask", myArgs , &reply)
+					if ok == false {
+						fmt.Printf("RPS Failure\n")
+
+						failChannel <- myArgs
+
+					} else {
+						mr.registerChannel <- myWorker
+					}
+				}(failTaskArgs, idle_worker)
+
+
+
+
+			default:
+				args := new(DoTaskArgs)
+				args.JobName = mr.jobName
+				args.File = mr.files[current_task]
+				args.Phase = phase
+				args.TaskNumber = current_task
+				args.NumOtherPhase = nios
+
+				current_task++
+				go func(myArgs *DoTaskArgs, myWorker string) {
+					
+
+
+					var reply ShutdownReply
+					ok := call(myWorker, "Worker.DoTask", myArgs , &reply)
+					if ok == false {
+						fmt.Printf("RPS Failure\n")
+						failChannel <- myArgs
+
+
+					} else {
+						mr.registerChannel <- myWorker
+					}
+
+
+				}(args, idle_worker)
+			}
 			
-			args := new(DoTaskArgs)
-			args.JobName = mr.jobName
-			args.File = mr.files[current_task]
-			args.Phase = phase
-			args.TaskNumber = current_task
-			args.NumOtherPhase = nios
-
-			current_task++
-			go func(myArgs *DoTaskArgs, myWorker string) {
-				
-
-
-				var reply ShutdownReply
-				ok := call(myWorker, "Worker.DoTask", myArgs , &reply)
-				if ok == false {
-					fmt.Printf("Cleanup: RPC %s error\n", mr.address)
-				}
-
-				mr.registerChannel <- myWorker
-
-			}(args, idle_worker)
-
-
 		default :
 
 		}
